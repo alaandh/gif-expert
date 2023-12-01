3778fba3d654080378c8990b27cbba42f5629dee
Alan Heizenreder <alanheizenreder01@gmail.com>
2023-12-01 19:23:23 -0300

    App Finished v 1.0

--- src/GifExpert.jsx
+++ src/GifExpert.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from "./components";
+
 /* eslint-disable */
 export const GifExpert = () => {
   const [categories, setCategories] = useState([""]);
@@ -13,14 +13,13 @@ export const GifExpert = () => {
   return (
     <>
       <h1>GifExpert</h1>
-
       <AddCategory onNewCategory={ondAddCategory} />
 
       {categories.map((category) => (
         <GifGrid key={category} category={category} />
       ))}
 
-      <small className="small">Creado por Alan Heizenreder</small>
+      <small>Creado por Alan Heizenreder.</small>
     </>
   );
 };
--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,26 +1,16 @@
+/* eslint-disable */
 import { useEffect, useState } from "react";
 import { GifItem } from "./GifItem";
-import { getGifs } from "../helpers/getGifs";
-/* eslint-disable */
+import { useFetchGifs } from "../hooks/useFetchGifs";
+// import { getGifs } from "../helpers/getGifs";
+
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    if (newImages) {
-      setImages(newImages);
-    } else {
-      console.error("No se pudieron obtener las imágenes");
-    }
-  };
-
-  useEffect(() => {
-    getImages();
-  }, [category]);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && <h4>Cargando...</h4>}
 
       <div className="card-grid">
         {images.map((image) =>
